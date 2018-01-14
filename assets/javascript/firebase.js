42c303262e75e8bb7d8b9663ea7bafad91006060
jelizabethscott <jescottphoto@gmail.com>
2018-01-14 11:28:13 -0500

    update files

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,14 @@
+// 1. Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCjd9rPQrnAnn_2j94r0xlaGfAQ6JobXA0",
+    authDomain: "gtbootcamp-cebbd.firebaseapp.com",
+    databaseURL: "https://gtbootcamp-cebbd.firebaseio.com",
+    projectId: "gtbootcamp-cebbd",
+    storageBucket: "gtbootcamp-cebbd.appspot.com",
+    messagingSenderId: "151496263351"
+  };
+  firebase.initializeApp(config);
+
+// Create a variable to reference the database
+var database = firebase.database();
+
